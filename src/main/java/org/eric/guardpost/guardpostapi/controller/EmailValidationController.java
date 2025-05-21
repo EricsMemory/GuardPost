80cba52b1768e5648bf636a773b99dc758d8fe59
EricsMemory <erics.memory@gmail.com>
2025-05-21 12:15:39 -0400

    Added general improvements and project setup refinements - Changed Lombok support

--- src/main/java/org/eric/guardpost/guardpostapi/controller/EmailValidationController.java
+++ src/main/java/org/eric/guardpost/guardpostapi/controller/EmailValidationController.java
@@ -1,12 +1,9 @@
 package org.eric.guardpost.guardpostapi.controller;
 
-import jakarta.validation.constraints.Email;
 import org.eric.guardpost.guardpostapi.model.EmailValidationResult;
 import org.eric.guardpost.guardpostapi.service.EmailValidationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/org/eric/guardpost/guardpostapi/model/EmailValidationResult.java
+++ src/main/java/org/eric/guardpost/guardpostapi/model/EmailValidationResult.java
@@ -1,10 +1,25 @@
 package org.eric.guardpost.guardpostapi.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 
 public class EmailValidationResult {
 
+    @Getter @Setter String email;
+    @Getter @Setter private String message;
+    @Getter @Setter private boolean valid;
 
+    //Constructor for Spring
     public EmailValidationResult() {}
+
+    /**
+     *
+     * @param email User input to validate.
+     * @param message Validation results message.
+     * @param valid Boolean true/false depending on validation result.
+     */
+
     public EmailValidationResult(String email, String message, boolean valid) {
         this.email = email;
         this.message = message;
