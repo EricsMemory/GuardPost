a8249650065cd26ea6427c9001ee45f40449fdc2
EricsMemory <erics.memory@gmail.com>
2025-05-21 13:11:49 -0400

    Added general improvements and project setup refinements - Changed Lombok support

--- src/main/java/org/eric/guardpost/guardpostapi/service/EmailValidationService.java
+++ src/main/java/org/eric/guardpost/guardpostapi/service/EmailValidationService.java
@@ -6,13 +6,18 @@ import java.util.regex.*;
 @Service
 public class EmailValidationService {
 
+    //Constructor for service class - does not need any parameters
     public EmailValidationService() {}
 
+    //Method for checking if boolean is valid against regex
     public boolean isValid(String email){
+
+        //If email is null or empty, return a false boolean
         if (email == null || email.isEmpty()) {
             return false;
         }
 
+        //Regex that email must abide by to be true
         String regex = "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" +
                 "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
 
