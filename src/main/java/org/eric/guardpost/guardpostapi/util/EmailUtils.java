8b0b11f6927bccef1b512ece3a4023c3f3faf93d
EricsMemory <erics.memory@gmail.com>
2025-06-19 21:28:03 -0400

    - Moved syntax validation to EmailUtils class for separation of logic and service.

--- src/main/java/org/eric/guardpost/guardpostapi/util/EmailUtils.java
+++ src/main/java/org/eric/guardpost/guardpostapi/util/EmailUtils.java
@@ -4,13 +4,18 @@ import java.util.regex.Pattern;
 
 /** Utility class with non-specific methods */
 public class EmailUtils {
-
-    /** Method for extracting domain out of a full email address */
+    /**
+     * Method for extracting domain out of a full email address
+     * If the email is null or does not contain an "@", extractDomain returns null.
+     * Otherwise, it continues to slice a substring of the email beginning at the character
+     * after the "@" symbol.
+     * extractDomain returns that slice.
+     */
     public static String extractDomain(String email){
         if (email == null || !email.contains("@")){
             return null;
         }
-        return email.substring(email.indexOf("@")+1);
+        return email.substring(email.indexOf("@")+ 1);
     }
 
     /**
@@ -21,13 +26,10 @@ public class EmailUtils {
      * and returns true only if the email matches the required pattern.
      */
     public static boolean syntaxCheck(String email){
-
-        // If email is null or empty, return a false boolean
         if (email == null || email.isEmpty()) {
             return false;
         }
 
-        //Regex that email must abide by to be true
         String regex = "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" +
                 "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
         return Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(email).matches();
